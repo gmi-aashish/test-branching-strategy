875f6954b48de90279419006855fc38b54229b00
Aashish Kumar <Aashish.Kumar@genmills.com>
2025-01-06 21:38:04 +0530

    test

--- new_file.py
+++ new_file.py
@@ -4,18 +4,27 @@ print("test merge conflicts")
 print("Am I in the conflict? Yes!")
 
 
-def badly_formatted_func(a, b, c):
-    print("badly formatted function")
+def badly_formatted_func(
+        a, b,         c):
+    print("badly formatted function"
+          )
 
     return a + b + c
 
 
 def badly_formatted_func_repeated(a, b, c):
-    print("badly formatted function")
+    print(
+        "badly formatted function"
+    )
 
     return a + b + c
+
+
 
 
 def badly_formatted_func_repeated(a, b, c):
     print("badly formatted function")
     return a + b + c
+
+
+
