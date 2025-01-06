f7095016441b52a3057334d0aba3473fe746fd22
Aashish Kumar <Aashish.Kumar@genmills.com>
2025-01-06 20:35:06 +0530

    test format-code bot

--- new_file.py
+++ new_file.py
@@ -11,3 +11,12 @@ def badly_formatted_func(a,b,c) :
 
 
 
+def badly_formatted_func_repeated(a,b,c) :
+    print(
+        'badly formatted function')
+    return a  +b +c
+
+def badly_formatted_func_repeated(a,b,c) :
+    print(
+        'badly formatted function')
+    return a  +b +c
