4111d93771bf0c0c33e38a4ca2003b4ed49bf6e8
Aashish Kumar <Aashish.Kumar@genmills.com>
2025-01-07 18:19:32 +0530

    revert

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
 print("Hellooooooooo World!")
-
-def  greet(name):
-    print(  "Hello, "+name+"! Hope you are having a great day!"     )
-    if   name=="Alice":print(   "Hi Alice! Long time no see!")
-    elif name=="Bob":    print( "Hey Bob! How's it going?")
-    else :print(   "Nice to meet you, "+name+"!")
-
