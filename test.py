7aa4cbe5d4b8b111086f17736e4ce1c085225a82
Aashish Kumar <Aashish.Kumar@genmills.com>
2025-01-08 15:46:55 +0530

    test no-looping on bot PR

--- test.py
+++ test.py
@@ -5,4 +5,8 @@ def  greet(name):
     else :print(   "Nice to meet you, "+name+"!")
 
 
-
+def  greet_again(name):
+    print(  "Hello, "+name+"! Hope you are having a great day!"     )
+    if   name=="Alice":print(   "Hi Alice! Long time no see!")
+    elif name=="Bob":    print( "Hey Bob! How's it going?")
+    else :print(   "Nice to meet you, "+name+"!")
