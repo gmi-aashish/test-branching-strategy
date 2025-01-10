8736facb8cd278775efea84da06dabef715b4cd4
Aashish Kumar <Aashish.Kumar@genmills.com>
2025-01-10 13:28:29 +0530

    bot PR to sync QA w/ main

--- new_file.py
+++ new_file.py
@@ -15,8 +15,6 @@ def badly_formatted_func_repeated(a, b, c):
 
     return a + b + c
 
-
-def badly_formatted_func_repeated_again(a, b, c):
+def badly_formatted_func_repeated_again(a,b,c):
     print("badly formatted function")
-
-    return a + b + c
+    return a +b +  c
