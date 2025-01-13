936912df9e22b9b105ccaf4772a4a36028c68d60
Aashish Kumar <Aashish.Kumar@genmills.com>
2025-01-13 12:12:57 +0530

    test allow merge on lint check passing

--- new_file.py
+++ new_file.py
@@ -14,3 +14,7 @@ def badly_formatted_func_repeated(a, b, c):
     print("badly formatted function")
 
     return a + b + c
+def badly_formatted_func_repeated_again(a,b,c):
+    print("badly formatted function")
+
+    return a +b + c
